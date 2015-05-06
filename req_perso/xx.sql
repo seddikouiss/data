7ad6fa893be92a2c454ad3b9189663c99171b7c4
unknown <souiss@SOUISS1.Smart-Trade.net>
2015-05-06 14:50:18 +0200

    ss

--- req_perso/xx.sql
+++ req_perso/xx.sql
@@ -4,4 +4,23 @@ use cube_st_from_st__db_v2 ;
  
  show tables ;
  
- select id_order_sell from trade_facts
+ select * from trade_facts;
+ select * from order_facts;
+ 
+ -- Pourcentages ....................
+ 
+select id_counter,(count(*)/O.total) *100 as 'percent of order' 
+from order_facts, (select count(*) as total from  order_facts) O
+group by id_counter;
+  
+select id_time,(count(*)/O.total) *100 as 'percent of order' 
+from order_facts, (select count(*) as total from  order_facts) O
+group by id_time;
+
+
+select id_product,(count(*)/O.total) *100 as 'percent of order' 
+from order_facts, (select count(*) as total from  order_facts) O
+group by id_product;
+
+
+select sum(contrat_amount) from order_facts ;
